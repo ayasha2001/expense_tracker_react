be36b1e3fda4274c32fe0efe34184b7ef439496f
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2023-12-22 13:21:50 +0530

    useState used

--- src/components/expense/ExpenseDetail.js
+++ src/components/expense/ExpenseDetail.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseDate.css";
 
 const ExpenseDetail = ({ prop }) => {
-  const { title, price, location } = prop;
-  const handleClick = (event) => {
+  const { title, location } = prop;
+  const [price, setPrice] = useState(prop.price)
+
+  const handleDelete = (event) => {
     const elementToRemove = event.target.closest('.card '); 
     // closest method to find the closest ancestor element with a specified class 
     if (elementToRemove) {
         elementToRemove.remove();
     }  };
+
+  const handlePriceUpdate = () => {
+      setPrice("rs 100")
+  }
+
   return (
     <div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <h2>{location}</h2>
         <div className="expense-item__price"> {price} </div>
-        <button onClick={handleClick}>delete item</button>
+        <button onClick={handleDelete}>delete item</button>
+        <button onClick={handlePriceUpdate}>update item</button>
       </div>
     </div>
   );
