eb9db1d4a277605e8acec8b48348200c9643ab67
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2023-12-22 21:40:52 +0530

    made the UI dynamic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Card from "./components/UI/Card";
 import ExpenseItem from "./components/expense/ExpenseItem";
 import NewExpense from "./components/newExpense/NewExpense";
@@ -6,33 +7,36 @@ function App() {
     {
       date: new Date(2023, 11, 20),
       title: "Food",
-      price: "Rs 10",
-      location: "Delhi",
+      price: "10",
+      id: 1,
     },
     {
       date: new Date(2023, 11, 21),
       title: "Petrol",
-      price: "Rs 200",
-      location: "Delhi",
+      price: "200",
+      id: 2,
     },
     {
       date: new Date(2023, 11, 21),
       title: "Movies",
-      price: "Rs 33",
-      location: "Delhi",
+      price: "33",
+      id: 3,
     },
   ];
+
+  const [allExpenses, setAllExpense] = useState(expenses);
+
+  const onNewExpenseAddition = (new_expense) => {
+    setAllExpense((prevExpense) => [...prevExpense, new_expense]);
+  };
+
   return (
     <div className="App">
       <h2>Expense Item!</h2>
       <Card className="expense container">
-        <NewExpense />
-        {expenses.map((expense, i) => {
-          return (
-            <Card className="expense">
-              <ExpenseItem prop={expense} key={i} />
-            </Card>
-          );
+        <NewExpense onNewExpenseAddition={onNewExpenseAddition} />
+        {allExpenses.map((expense, i) => {
+          return <ExpenseItem prop={expense} key={i} />;
         })}
       </Card>
     </div>
